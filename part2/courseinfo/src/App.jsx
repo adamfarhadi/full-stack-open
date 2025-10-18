89117e3621751965faa956db8c5d1cdaa0ad29ca
Adam Farhadi <adam@adamfarhadi.com>
2025-10-18 18:07:26 +0200

    exercises 2.1 to 2.3 done

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </>
+  )
+}
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Total = ({ total }) => <p><b>total of {total} exercises</b></p>
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
