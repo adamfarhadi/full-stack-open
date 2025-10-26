79a315e8867c525909480f849a15c6c55c26d445
Adam Farhadi <adam@adamfarhadi.com>
2025-10-26 13:50:16 +0100

    exercise 3.17 done

--- part3/phonebook/phonebook-backend/index.js
+++ part3/phonebook/phonebook-backend/index.js
@@ -88,6 +88,23 @@ app.post('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      if (!person)
+        return response.status(404).end()
+
+      person.name = name
+      person.number = number
+
+      return person.save().then(updatedPerson => response.json(updatedPerson))
+    })
+    .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
   console.error('error.message: ', error.message)
   console.error('error.name: ', error.name)
