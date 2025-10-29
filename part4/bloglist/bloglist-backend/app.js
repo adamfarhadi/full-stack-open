7ec12152046f2c74e0478a55efca43de21d3e1f8
Adam Farhadi <adam@adamfarhadi.com>
2025-10-29 11:04:18 +0200

    added logger

--- part4/bloglist/bloglist-backend/app.js
+++ part4/bloglist/bloglist-backend/app.js
@@ -3,20 +3,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const app = express()
 
 console.log('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
 app.use(express.json())
 
-app.use('/api/blogs',blogsRouter)
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
