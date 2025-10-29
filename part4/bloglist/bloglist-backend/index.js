c241c7bb813bbdc136cce9c7760e3507e6964785
Adam Farhadi <adam@adamfarhadi.com>
2025-10-29 10:51:04 +0200

    added blogs controller and blog model for exercise 4.2

--- part4/bloglist/bloglist-backend/index.js
+++ part4/bloglist/bloglist-backend/index.js
@@ -1,17 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const url = process.env.MONGODB_URI
 
@@ -26,19 +18,7 @@ mongoose.connect(url)
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs',blogsRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
