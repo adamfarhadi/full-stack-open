9d351220e22d8c3f83f9ba4a9262260266bb4704
Adam Farhadi <adam@adamfarhadi.com>
2025-11-04 16:46:33 +0200

    4.21 done

--- part4/bloglist/bloglist-backend/controllers/blogs.js
+++ part4/bloglist/bloglist-backend/controllers/blogs.js
@@ -38,7 +38,33 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(204).end()
+  }
+
+  if (blog.user.toString() !== user._id.toString()) {
+    return response.status(403).json({ error: 'user is unauthorized to perform the operation' })
+  }
+
+  await blog.deleteOne()
+
+  user.blogs = user.blogs.filter(b => b._id.toString() !== blog._id.toString())
+  await user.save()
+
   response.status(204).end()
 })
 
