5cdadceeaf537240c887498e1c6270937c901e1f
Adam Farhadi <adam@adamfarhadi.com>
2025-11-04 15:31:47 +0200

    ex 4.18 and 4.19 done

--- part4/bloglist/bloglist-backend/controllers/blogs.js
+++ part4/bloglist/bloglist-backend/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const User = require('../models/user')
 const Blog = require('../models/blog')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -10,10 +19,16 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const user = await User.findOne()
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
-    return response.status(400).json({ error: 'user not found' })
+    return response.status(400).json({ error: 'userId missing or not valid' })
   }
 
   const blog = new Blog({
