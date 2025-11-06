f2548e42f534afe770124cea23f52a38795101d3
Adam Farhadi <adam@adamfarhadi.com>
2025-11-06 11:11:57 +0200

    ex 5.1 done

--- part5/bloglist/bloglist-frontend/src/App.jsx
+++ part5/bloglist/bloglist-frontend/src/App.jsx
@@ -1,22 +1,80 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
-  return (
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      console.error('error logging in')
+    }
+  }
+
+  const blogForm = () => (
     <div>
       <h2>blogs</h2>
+      {
+        <p>
+          {user.username} logged in
+        </p>
+      }
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  const loginForm = () => (
+    <div>
+      <h1>log in to application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  return (
+    <div>
+      {!user && loginForm()}
+      {user && blogForm()}
     </div>
   )
 }
