bcb2f324747c1015416c8221e77c2443c493c6fa
Adam Farhadi <adam@adamfarhadi.com>
2025-11-10 14:15:01 +0100

    ex 5.18 done

--- part5/bloglist/bloglist-e2e-tests/tests/blog_app.test.js
+++ part5/bloglist/bloglist-e2e-tests/tests/blog_app.test.js
@@ -1,4 +1,6 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
+
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -19,5 +21,21 @@ describe('Blog app', () => {
     await expect(page.getByRole('textbox', { name: 'password' })).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
     await expect(page.getByText('logged in')).not.toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'salainen')
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'wrong')
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('wrong username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+    })
   })
 })
