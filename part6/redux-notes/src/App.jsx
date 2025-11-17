d85fc5f7f704eb7b3ab11541d7b8a3cf244729fd
Adam Farhadi <adam@adamfarhadi.com>
2025-11-17 14:58:23 +0100

    redux-notes done up to ex 6.14

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,8 +1,28 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import NoteForm from './components/NoteForm'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import { setNotes } from './reducers/noteReducer'
+import noteService from './services/notes'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const test = await noteService.getAll()
+        dispatch(setNotes(test))
+      } catch (error) {
+        console.error('Error: ', error)
+      }
+    }
+
+    fetchNotes()
+  }, [dispatch])
+
   return (
     <div>
       <NoteForm />
