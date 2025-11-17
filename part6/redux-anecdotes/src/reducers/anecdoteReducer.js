c99e47012d8d21fa5a66fb1f7125907853087238
Adam Farhadi <adam@adamfarhadi.com>
2025-11-17 15:24:37 +0100

    ex 6.15 done

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,13 @@
-import { createSlice, current } from "@reduxjs/toolkit"
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = anecdote => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+import { createSlice } from "@reduxjs/toolkit"
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      console.log('createAnecdote current state: ', current(state))
       const content = action.payload
-      state.push(asObject(content))
+      console.log('createAnecdote content: ', content)
+      state.push(content)
     },
     voteForAnecdote(state, action) {
       const id = action.payload
